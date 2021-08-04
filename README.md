a8b5ccc8c4df9ddae69c9ceb5986510a9470b676
Zoya Tanweer <87615284+zoyatanweer@users.noreply.github.com>
2021-08-04 20:02:14 +0530

    Banana-talk

---
+++ README.md
@@ -0,0 +1,3 @@
+#Banana talk
+
+Hi guys! Check out my app which converts english language into minion language.
---
+++ app.js
@@ -0,0 +1,24 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv= document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+   return serverURL + "?" +"text=" + text;
+}
+function errorHandler(error){
+   console.log("error occured",error);
+   alert("something wrong with the server, try again next time");
+}
+
+function clickHandler(){
+   var inputText= txtInput.value    /**taking input**/
+   fetch(getTranslationURL(inputText))
+      .then (response => response.json())
+      .then (json => {
+         var translatedText=json.contents.translated;
+         outputDiv.innerText = translatedText;
+      })
+      .catch(errorHandler)
+};
+btnTranslate.addEventListener("click", clickHandler)
