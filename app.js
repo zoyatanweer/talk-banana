9f06572d704829d7761cd6238705db44a8ef30f3
Zoya Tanweer <87615284+zoyatanweer@users.noreply.github.com>
2022-01-10 18:28:09 +0530

    refactor code
    converted the functions into arrow functions

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv= document.querySelector("#output");
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslationURL(text){
-   return serverURL + "?" +"text=" + text;
-}
-function errorHandler(error){
+const getTranslationURL = (text) => serverURL + "?" + "text=" + text
+
+const errorHandler = (error) => {
    console.log("error occured",error);
    alert("something wrong with the server, try again next time");
 }
